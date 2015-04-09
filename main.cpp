d3fc35c956001bf18562f07de7d804f6a879d108
Yang Yang <YYang@Yangs-MBP.memphis.edu>
2015-04-09 10:17:10 -0500

    new version of main function

--- main.cpp
+++ main.cpp
@@ -1,14 +1,32 @@
+/******************************************************************************
+-   earthquake.h
+-   Prog Tools for Scits Engrs - CIVL 8903 Homework IX
+-
+-   File Discreption: main function
+-
+-   Implemented by all of us
+-
+-   Github Repository: https://github.com/SwiftHickory/homework8.git
+******************************************************************************/
+
 #include "event.h"
 
 using namespace std;
 
 int main(int argc, char *argv[]) {
 
-	list<Event> evDB;
-
-	while (inputFileName) {
-		Event tempEvent(inputFileName);
-		evDB.push_back(tempEvent);
+	if (argc > 1) {
+		Event *eventDB = new Event[argc - 1];
+	} else {
+		cout << "Usage: " << argv[0] << " fileName1 fileName2 ..." << end;
+		cout << "Please give at least one input file" << endl;
+		return;
 	}
+	
+	for (int i = 0; i < argc - 1; i++) {
+		eventDB[i].setInputFile(argv[i]);
+	}
+
+	return;
 
 }
